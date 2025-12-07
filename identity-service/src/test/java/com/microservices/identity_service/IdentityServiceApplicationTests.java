aa2f4a4c45ccee4825a5ba15ae6997f9249cab0d
Rajasekhar Reddy <rajasekharreddy9581@gmail.com>
2025-12-07 14:43:01 +0530

    first test case

--- identity-service/src/test/java/com/microservices/identity_service/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/com/microservices/identity_service/IdentityServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.microservices.identity_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
+@Slf4j
 class IdentityServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+        log.info("Test case execute");
+        assertTrue(true);
 	}
 
 }
